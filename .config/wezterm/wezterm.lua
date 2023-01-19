7e4d02a48f3946e2132a6fe85b44b2eb132919da
Tommy Morgan <tommy@tommymorgan.name>
2023-01-18 21:02:12 -0500

    Getting started with a wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require 'wezterm'
+return {
+  color_scheme = 'Dracula',
+  enable_scroll_bar = false,
+  font = wezterm.font 'FiraCode Nerd Font',
+  font_size = 16,
+  harfbuzz_features = { 'ss02', 'ss06', 'ss07', 'ss08', 'zero' },
+  hide_tab_bar_if_only_one_tab = true,
+  window_decorations = 'RESIZE',
+  window_frame = {
+    font = wezterm.font { family = 'FiraCode Nerd Font', weight = 'Bold'},
+    font_size = 13,
+  },
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+}
